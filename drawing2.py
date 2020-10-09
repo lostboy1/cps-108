3ac90dd60746fe3eaadbc93089c39fdbc8d1e32e
Olga Penkovskaia <olgapenkovskaia@MacBook-Pro-2.local>
2020-10-09 10:53:29 -0400

    wednesday class

--- drawing2.py
+++ drawing2.py
@@ -14,19 +14,69 @@ c.pack()
 # c.create_rectangle(375, 0, 437.5, 62.5, fill='grey')
 # c.create_rectangle(437.5, 0, 500, 62.5, fill='white')
 
-for x in (0, 125):
+#first row 
+for x in range (0,500, 125):
     c.create_rectangle(x, 0, x+62.5, 62.5, fill='grey')
     c.create_rectangle(x+62.5, 0, x+125, 62.5, fill='white')
 
+#second row
+for x in range (0, 500, 125):
+    c.create_rectangle(x, 62.5, x+62.5, 125, fill='white')
+    c.create_rectangle(x+62.5, 62.5, x+125, 125, fill='grey')
 
+#third row
+for x in range (0, 500, 125):
+    c.create_rectangle(x, 125, x+62.5, 187.5, fill='grey')
+    c.create_rectangle(x+62.5, 125, x+125, 187.5, fill='white')
 
-for y in range(50, 500, 50):
-    c.create_line(0, y, 500, y)
-    c.create_text(18, y + 10, text=str(y))
+#forth row
+for x in range (0, 500, 125):
+    c.create_rectangle(x, 187.5, x+62.5, 250, fill='white')
+    c.create_rectangle(x+62.5, 187.5, x+125, 250, fill='grey')
 
-for x in range(50, 500, 50):
-    c.create_line(x, 0, x, 500)
-    c.create_text(x + 18, 10, text=str(x))
+#fifth row
+for x in range (0, 500, 125):
+    c.create_rectangle(x, 250, x+62.5, 312.5, fill='grey')
+    c.create_rectangle(x+62.5, 250, x+125, 312.5, fill='white')
+
+#sixth row
+for x in range (0, 500, 125):
+    c.create_rectangle(x, 312.5, x+62.5, 375, fill='white')
+    c.create_rectangle(x+62.5, 312.5, x+125, 375, fill='grey')
+
+#seventh row
+for x in range (0, 500, 125):
+    c.create_rectangle(x, 375, x+62.5, 437.5, fill='grey')
+    c.create_rectangle(x+62.5, 375, x+125, 437.5, fill='white')
+
+#eightth row
+for x in range (0, 500, 125):
+    c.create_rectangle(x, 437.5, x+62.5, 500, fill='white')
+    c.create_rectangle(x+62.5, 437.50, x+125, 500, fill='grey')
+
+#first row circles
+for x in range (0, 500, 125):
+    c.create_oval(x+5, 5, x+57.5, 57.5, fill='red')
+
+#second row circles
+for x in range (0, 500, 125):
+    c.create_oval(x+67.5, 67.5, x+119.5, 119.5, fill='red')
+
+#third row circles
+for x in range (0, 500, 125):
+    c.create_oval(x+5, 130, x+57.5, 182.5, fill='red')
+
+#first row circles
+for x in range (0, 500, 125):
+    c.create_oval(x+67.5, 442.5, x+119.5, 495, fill='blue')
+
+#second row circles
+for x in range (0, 500, 125):
+    c.create_oval(x+5, 380.5, x+57.5, 432.5, fill='blue')
+
+#third row circles
+for x in range (0, 500, 125):
+    c.create_oval(x+67.5, 317.5, x+119.5, 370, fill='blue')
 
 def react_to_click(event):
     root.quit()
